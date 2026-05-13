07ba764b1fe1b9d0c0866f41ac76aad940cc6e73
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-05-13 08:05:12 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int n,m;
+    int dir[4][2] = {{-1,0},{0,-1},{1,0},{0,1}};
+    void solve(int i,int j, vector<vector<int>>& grid,int& area) {
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j] == 0) {
+            return;
+        }
+        grid[i][j] = 0;
+
+        for(int k=0;k<4;k++) {
+            int nr = i + dir[k][0];
+            int nc = j + dir[k][1];
+
+            if(nr>=0 && nc>=0 && nr<n && nc<m && grid[nr][nc] == 1) {
+                area++;
+                solve(nr,nc,grid,area);
+            }
+        }
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        n = grid.size(), m = grid[0].size();
+
+        int area = 0;
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(grid[i][j] == 1) {
+                    int currArea = 1;
+                    solve(i,j,grid,currArea);
+                    area = max(area,currArea);
+                }
+            }
+        }
+        return area;
+    }
+};
