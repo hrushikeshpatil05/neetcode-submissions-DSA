ba28564e10ca2c387d7c6dd6172cbed141b4bd4e
hrushikeshpatil05 <78201965+hrushikeshpatil05@users.noreply.github.com>
2026-05-21 23:02:38 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int n;
+    int numDecodings(string s) {
+        n = s.size();
+        if(n == 0 || s[0] == '0') {
+            return 0;
+        }
+
+        vector<int>dp(n+1,0);
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++) {
+            int singleDigit = s[i-1] - '0';
+            if(singleDigit >= 1 && singleDigit <= 9) {
+                dp[i] += dp[i-1];
+            }
+            int doubleDigit = (s[i-2] - '0')*10 + singleDigit;
+            if(doubleDigit >= 10 && doubleDigit <= 26) {
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+};
